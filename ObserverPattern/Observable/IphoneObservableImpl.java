017fe929209da0c11c4b1990920c9215b5813b97
Prafful Chauhan <praffulchauhan49@gmail.com>
2024-03-11 07:53:27 +0530

    Observer Pattern

---
+++ ObserverPattern/Observable/IphoneObservableImpl.java
@@ -0,0 +1,42 @@
+package Observable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Observer.NotificationAlertObserver;
+
+public class IphoneObservableImpl implements StockObservable {
+    public List<NotificationAlertObserver> observerList=new ArrayList<>();
+    public int stockCount=0;
+
+
+    @Override
+    public void add(NotificationAlertObserver observer) {
+        observerList.add(observer);
+    }
+
+    @Override
+    public void remove(NotificationAlertObserver observer) {
+        observerList.remove(observer);
+    }
+
+    @Override
+    public void notifySubscribers() {
+        for(NotificationAlertObserver observer:observerList){
+            observer.update();
+        }
+    }
+
+    @Override
+    public void setStockCount(int newStockAdded) {
+        if(stockCount==0){
+            notifySubscribers();
+        }
+        stockCount=stockCount+newStockAdded;
+    }
+
+    @Override
+    public int getStockCount() {
+        return stockCount;
+    }
+}
---
+++ ObserverPattern/Observer/MobileAlertObserverImpl.java
@@ -0,0 +1,23 @@
+package Observer;
+
+import Observable.StockObservable;
+
+public class MobileAlertObserverImpl implements NotificationAlertObserver {
+	String userName;
+    StockObservable stockObservable;
+
+    public MobileAlertObserverImpl(String emailId,StockObservable stockObservable){
+        this.userName=emailId;
+        this.stockObservable=stockObservable;
+    }
+
+
+    @Override
+    public void update() {
+        sendMsgOnMobile(userName,"Product is in stock hurry up!!!");
+    }
+
+    public void sendMsgOnMobile(String userName,String msg){
+        System.out.println("msg sent to : "+userName);
+    }
+}
